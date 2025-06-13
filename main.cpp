cca28a31e24902ac20f9b1bebf411608664b7268
Sebastian Musial <dev.sebas.musial@gmail.com>
2025-06-13 22:42:01 +0200

    Add "Top up account" function

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ class Klient {
 
 class KlientVIP : public Klient {
     double dodatkoweOprocentowanie;
-}
+};
 
 //Dodawanie nowych klientów - Nowy klient saldo = 0, oprocentowanie = 2%
 Klient Add_Client() {
@@ -26,7 +26,19 @@ Klient Add_Client() {
     Temporary.Oprocentowanie = 1.02;
     return Temporary;
 }
+
 //Zasilenie konta klienta
+void Top_Up_Account(Klient& X)  {
+    int T_Value;
+    cout<<"Wplacasz pieniadze na konto"<<endl;
+    do {
+        cout<<"Podaj kwote wplaty: ";
+        cin>>T_Value;
+        if (T_Value<=0) cout<<"Kwota musi byc dodatnia. Sprobuj jeszcze raz"<<endl;
+    } while (T_Value <= 0);
+    X.Saldo_Konta += T_Value;
+}
+
 //Wypłata z konta klienta - do wysokości salda
 //Przelew pomiędzy dwoma istniejącymi kontami
 //Naliczenie oprocentowania dla konta
