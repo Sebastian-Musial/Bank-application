3b5279bb4d584997070733ced9944848bb5a0269
Sebastian Musial <dev.sebas.musial@gmail.com>
2025-06-13 22:23:11 +0200

    Add "Add Client" function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 class Klient {
@@ -13,7 +14,18 @@ class KlientVIP : public Klient {
     double dodatkoweOprocentowanie;
 }
 
-//Dodawanie nowych klientów
+//Dodawanie nowych klientów - Nowy klient saldo = 0, oprocentowanie = 2%
+Klient Add_Client() {
+    Klient Temporary;
+    cout<<"Dodajesz wlasnie nowego klienta"<<endl;
+    cout<<"Podaj Imie: ";
+    cin>>Temporary.Imie;
+    cout<<"Podaj nazwisko: ";
+    cin>>Temporary.Nazwisko;
+    Temporary.Saldo_Konta = 0;
+    Temporary.Oprocentowanie = 1.02;
+    return Temporary;
+}
 //Zasilenie konta klienta
 //Wypłata z konta klienta - do wysokości salda
 //Przelew pomiędzy dwoma istniejącymi kontami
